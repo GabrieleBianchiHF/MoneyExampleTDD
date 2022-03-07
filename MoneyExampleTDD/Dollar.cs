d5b883860fcf994d038cbead8c4db8ac537e6e56
Bianchi Gabriele <8046136@itwconnect.com>
2022-03-07 11:32:37 +0100

    Times imple 2

--- MoneyExampleTDD/Dollar.cs
+++ MoneyExampleTDD/Dollar.cs
@@ -17,8 +17,7 @@ namespace MoneyExampleTDD
 
         public Dollar Times(int multiplier)
         {
-            amount *= multiplier;
-            return null;
+            return new Dollar(amount * multiplier);
         }
     }
 }
