c8109bda7e25db93faa7ccdf23313aaf7f07ad16
Bianchi Gabriele <8046136@itwconnect.com>
2022-03-08 15:00:53 +0100

    Factory method returns Base class

--- MoneyExampleTDD/Money.cs
+++ MoneyExampleTDD/Money.cs
@@ -36,12 +36,12 @@ namespace MoneyExampleTDD
 
         public static Money MakeDollars(int amount)
         {
-            return new Dollar(amount, "USD");
+            return new Money(amount, "USD");
         }
 
         public static Money MakeFrancs(int amount)
         {
-            return new Franc(amount, "CHF");
+            return new Money(amount, "CHF");
         }
     }
 }
--- MoneyTests/MoneyTests.cs
+++ MoneyTests/MoneyTests.cs
@@ -48,12 +48,6 @@ namespace MoneyTests
         }
 
         [Fact]
-        public void TestDifferentClassEquality()
-        {
-            new Money(10, "CHF").Equals(new Franc(10, "CHF")).Should().BeTrue(because: "Are different class but have the same obj.");
-        }
-
-        [Fact]
         public void TestCurrency()
         {
             string dollarCurrency = "USD";
